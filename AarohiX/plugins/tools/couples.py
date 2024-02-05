6f07fa38796ae6638fb2bf5801b396bd180c0d08
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-02-05 09:40:18 +0530

    Update couples.py

--- AarohiX/plugins/tools/couples.py
+++ AarohiX/plugins/tools/couples.py
@@ -1,7 +1,7 @@
 import os 
 import random
 from datetime import datetime 
-from graph import upload_file
+from telegraph import upload_file
 from PIL import Image , ImageDraw
 from pyrogram import *
 from pyrogram.types import *
